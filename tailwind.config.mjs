6822ea1d0de535072f4443c22738d1d547954eaa
Mominul Islam <mominulfed@gmail.com>
2024-12-12 10:45:52 +0600

    Intial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+			colors: {
+			
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+			},
+			container: {
+				center: true,
+				maxWidth: {
+					DEFAULT: '100%',
+					sm: '640px',
+					md: '768px',
+					lg: '1024px',
+					xl: '1280px',
+					'2xl': '1536px',
+				},
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '4rem',
+					xl: '5rem',
+					'2xl': '6rem',
+				},
+			},
+    	}
+    },
+	plugins: [require("tailwindcss-animate")],
 }
